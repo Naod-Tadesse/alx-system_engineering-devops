7a7971909321d3bcb8eb9b74d321901875401b05
Naod Taddese <naodtds@gmail.com>
2024-02-23 22:33:37 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # this script installs flask using pip 3
 
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'python3-pip' }
+
+pip {
+  name   => 'Flask',
+  ensure => '2.1.0',
   provider => 'pip3'
 }
