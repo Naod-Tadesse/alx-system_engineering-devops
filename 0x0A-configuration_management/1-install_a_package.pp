14c79cef64ef91b6f0929477466124ecdc5a5f45
Naod Taddese <naodtds@gmail.com>
2024-02-23 21:25:41 +0300

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # this script installs flask using pip 3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'installflask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
