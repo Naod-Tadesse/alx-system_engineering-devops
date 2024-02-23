f0285a973980ddcb61259afb817f901603df4ae6
Naod Taddese <naodtds@gmail.com>
2024-02-23 20:40:05 +0300

    style task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # this script creates file school in tmp folder
 
-file { "/tmp/school":
-    content: 'I love Puppet',
-	owner: 'www-data',
-	mode: '0744',
-	group: 'www-data'
+file { '/tmp/school':
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  mode    => '0744',
+  group   => 'www-data'
 }
