7be74b0ec65a335e01c77f69e440ff2c551c9b52
Naod Taddese <naodtds@gmail.com>
2024-02-27 19:21:39 +0300

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,15 @@
-# this modifies the config
-file {'/etc/ssh/ssh_config':
+# config file for ssh config file
+
+file_line { 'remove password':
   ensure  => present,
-  content => "
-    PasswordAuthentication no
-    IdentityFile ~/.ssh/school
-  "
+  path    => '/etc/ssh/ssh_config',
+  line    => '	PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'for publickey':
+  ensure  =>  present,
+  path    =>  '/etc/ssh/ssh_config',
+  line    =>  '	IdentityFile ~/.ssh/school',
+  replace => true
 }
