f0388b2ebb0eda9bd835bd017b7ac916ba56bca9
Naod Taddese <naodtds@gmail.com>
2024-03-04 23:49:10 +0300

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,19 +8,7 @@ exec {'sudo apt-get -y install nginx':
   provider => shell
 }
 
-exec {'sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default':
-  provider => shell
-}
-
-exec {'sed -i "/listen 80 default_serve;/a rewrite ^/redirect_me https://linkedin.com permanent;" /etc/nginx/sites-available/default':
-  provider => shell
-}
-
-exec {'sed -i "/listen 80 default_serve;/a rewrite ^/redirect_me https://linkedin.com permanent;" /etc/nginx/sites-available/default':
-  provider => shell
-}
-
-exec {'sed -i "/listen 80 default_server;/a error_page 404 /notfound_404.html;" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default':
   provider => shell
 }
 
