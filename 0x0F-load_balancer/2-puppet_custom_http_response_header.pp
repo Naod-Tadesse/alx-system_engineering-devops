ddb323504c77b82d2ff20075c53663daad5d71aa
Naod Taddese <naodtds@gmail.com>
2024-03-04 23:26:52 +0300

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+# use ha proxy using puppet
+
+exec {'apt-get update':
+  provider => shell,
+}
+
+exec {'apt-get install -y nginx':
+  provider => shell,
+}
+
+exec {'sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default':
+  provider => shell
+}
+
+exec {'sed -i "/listen 80 default_serve;/a rewrite ^/redirect_me https://linkedin.com permanent;" /etc/nginx/sites-available/default':
+  provider => shell
+}
+
+exec {'sed -i "/listen 80 default_serve;/a rewrite ^/redirect_me https://linkedin.com permanent;" /etc/nginx/sites-available/default':
+  provider => shell
+}
+
+exec {'sed -i "/listen 80 default_server;/a error_page 404 /notfound_404.html;" /etc/nginx/sites-available/default':
+  provider => shell
+}
+
+exec {'sudo service nginx restart':
+  provider => shell
+}
