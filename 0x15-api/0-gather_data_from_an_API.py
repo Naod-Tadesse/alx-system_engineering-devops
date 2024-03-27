2ee3ef5ed83de5c49bf4e78d83f225f2c03b60ed
Naod Taddese <naodtds@gmail.com>
2024-03-27 10:34:22 +0300

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+def todo_progress():
+    url = "https://jsonplaceholder.typicode.com"
+    response = requests.get(f"{url}/users/{sys.argv[1]}")
+
+    employee_data = response.json()
+    employee_name = employee_data['name']
+
+    todo_response = requests.get(f"{url}/todos?userId={sys.argv[1]}")
+
+    todos = todo_response.json()
+
+    total_tasks = len(todos)
+    done_tasks = sum(1 for todo in todos if todo['completed'])
+
+    print(
+        f"Employee {employee_name} is done with \
+tasks({done_tasks}/{total_tasks}):")
+    for todo in todos:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
+
+
+if __name__ == "__main__":
+    todo_progress()
