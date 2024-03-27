dddf3421d24027cd9cb5b53111cd7109ddde7f30
Naod Taddese <naodtds@gmail.com>
2024-03-27 15:03:11 +0300

    task 1 done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""csv export"""
-import csv
+"""get users task progress and export to CSV"""
 import requests
+import csv
 import sys
 
 
 def todo_progress():
-    """csv export"""
+    """get the tasks progress and export to CSV"""
     url = "https://jsonplaceholder.typicode.com"
     user_id = sys.argv[1]
     response = requests.get(f"{url}/users/{user_id}")
 
     employee_data = response.json()
-
-    ename = employee_data['username']
+    ename = employee_data['name']
 
     todo_response = requests.get(f"{url}/todos?userId={user_id}")
 
     todos = todo_response.json()
 
+    tt = len(todos)
+    dt = sum(1 for todo in todos if todo['completed'])
+
     with open(f"{user_id}.csv", 'w', newline='') as csvfile:
         fieldnames = [
             'USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
             'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
 
         writer.writeheader()
         for todo in todos:
             writer.writerow({
                 'USER_ID': user_id,
                 'USERNAME': ename,
-                'TASK_COMPLETED_STATUS': todo["completed"],
-                'TASK_TITLE': todo["title"]
+                'TASK_COMPLETED_STATUS': todo['completed'],
+                'TASK_TITLE': todo['title']
             })
 
 
