61d3127a960f9e03a7507d212489358ddd55aa2b
Naod Taddese <naodtds@gmail.com>
2024-03-27 14:46:10 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,23 @@
 #!/usr/bin/python3
-"""get users task progress and export"""
+"""extend your Python script to export data in the CSV format"""
 import csv
 import requests
 import sys
 
 
-def todo_progress():
-    """get the tasks progress and export"""
-    url = "https://jsonplaceholder.typicode.com"
-    user_id = sys.argv[1]
-    response = requests.get(f"{url}/users/{user_id}")
-
-    employee_data = response.json()
-    ename = employee_data['name']
-
-    todo_response = requests.get(f"{url}/todos?userId={user_id}")
-
-    todos = todo_response.json()
-
-    tt = len(todos)
-    dt = sum(1 for todo in todos if todo['completed'])
-
-    with open(f"{user_id}.csv", 'w', newline='') as csvfile:
-        fieldnames = [
-            'USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
-            'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for todo in todos:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': ename,
-                'TASK_COMPLETED_STATUS': todo["completed"],
-                'TASK_TITLE': todo["title"]
-            })
-
-
 if __name__ == "__main__":
-    todo_progress()
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_response = requests.get("{}/users/{}".format(url, employee_id))
+    todos_response = requests.get("{}/todos?userId={}".
+                                  format(url, employee_id))
+
+    employee_name = user_response.json().get('username')
+
+    todos = todos_response.json()
+
+    with open("{}.csv".format(employee_id), mode='w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([employee_id, employee_name,
+                             task['completed'], task['title']])
