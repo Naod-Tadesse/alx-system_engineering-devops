9c1a330213c9c4622cb3accbef8f8d89d8df143e
Naod Taddese <naodtds@gmail.com>
2024-04-10 15:52:47 +0300

    task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""recursively fetch"""
+from requests import get
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """fetch recursively"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    global after
+    agent = {'User-Agent': 'My User custom 2.0'}
+    p = {'limit': 100, 'after': after}
+    result = get(url, headers=agent, params=p,
+                          allow_redirects=False)
+    if result.status_code == 200:
+        res = result.json()
+        dat = res.get('data')
+        children = dat.get('children')
+        after = dat.get('after')
+        for i in children:
+            hot_list.append(i.get('data').get('title'))
+        if after is not None:
+            recurse(subreddit, hot_list)
+    else:
+        return None
+    return hot_list
