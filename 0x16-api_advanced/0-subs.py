dac85428130563590cf09f76dfe8c0d835ffd733
Naod Taddese <naodtds@gmail.com>
2024-04-10 10:47:47 +0300

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""for a subreddit subs"""
-from requests import get
+"""subreddit"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "PostmanRuntime/7.37.3"}
-    response = get(url, headers=headers)
+    """sub reddits"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        res = response.json()
-        return res["data"]["subscribers"]
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
         return 0
