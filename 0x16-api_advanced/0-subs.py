e840540599af54be33c7114579c4bcbf27d16797
Naod Taddese <naodtds@gmail.com>
2024-04-10 10:30:49 +0300

    another try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """sub reddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'PostmanRuntime/7.37.3'}
-    result = requests.get(url, headers=headers).json()
+    agent = {'User-Agent': 'PostmanRuntime/7.37.3'}
+    result = requests.get(url, headers=agent)
+    data = result.json()
     try:
-        return result.get('data').get('subscribers')
+        return data.get('data').get('subscribers')
     except Exception:
         return 0
