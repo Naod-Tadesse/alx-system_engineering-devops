4df02bea394d62e720ccc858ecfb863580deaf2d
Naod Taddese <naodtds@gmail.com>
2024-04-11 15:00:16 +0300

    remove sudo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 $settings = 'www/html/wp-settings.php'
 
 exec { 'fix wordpress apache issue':
-  command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/bin'
 }
