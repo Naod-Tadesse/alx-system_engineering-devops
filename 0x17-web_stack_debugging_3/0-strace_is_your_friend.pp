38eae99fe38c867435bbecb74bd5fc7d6b8e034d
Naod Taddese <naodtds@gmail.com>
2024-04-11 14:54:08 +0300

    stack

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # fix bug in wordpress site
 $settings = 'www/html/wp-settings.php'
 
-exec { 'wordpess site fix':
-  command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+exec { 'fix the type phpp->php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/bin'
 }
