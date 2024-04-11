f840c1181b33001fc7eaa4863cbd5bb9422a5834
Naod Taddese <naodtds@gmail.com>
2024-04-11 14:52:11 +0300

    donezo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 $settings = 'www/html/wp-settings.php'
 
 exec { 'wordpess site fix':
-  command => "sudo sed -i 's/phpp/php/g' /var/${settings}",
+  command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/bin'
 }
