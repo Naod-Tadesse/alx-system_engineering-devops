113f8f8b6ae9fd370dd51bc63e31788b8ea9f149
Naod Taddese <naodtds@gmail.com>
2024-04-18 22:12:08 +0300

    task 0 fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,3 @@
-# Define the sed command to change the file descriptors in the nginx default configuration file
-exec { 'change_nginx_file_descriptors':
-  command     => '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx',
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  refreshonly => true, # Only run when notified
-}
-
-# Define a service resource to restart the nginx service when necessary
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Exec['change_nginx_file_descriptors'], # Subscribe to the exec resource
-}
-
+# Define the sed command to change the file des
+exec { '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
