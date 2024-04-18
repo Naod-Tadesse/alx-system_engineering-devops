9a09ca830d610a75107902a6f2af71e85afb863e
Naod Taddese <naodtds@gmail.com>
2024-04-18 22:50:31 +0300

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,3 @@
 # Define the sed command to change the file des
-exec { 'update_nginx_config':
-  command     => '/usr/bin/sed -i s/15/4096/ /etc/default/nginx',
-  refreshonly => true,
-  notify      => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-  command     => '/usr/bin/env service nginx restart',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-}
-
+exec { '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
